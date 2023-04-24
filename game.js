4e2cc08c6d0aeab849aac6b85a98087c0c3b5eb1
lelao22 <78011423+lelao22@users.noreply.github.com>
2023-04-24 20:30:10 -0300

    Update game.js

--- game.js
+++ game.js
@@ -1,56 +1,54 @@
 const rollButton = document.getElementById('roll-button');
 rollButton.addEventListener('click', function() {
-    const dice = document.querySelector('#dice-board .dice');
-  const betType = document.querySelector('input[name="bet-type"]:checked').value;
-  const betAmount = document.getElementById('bet-amount').value;
-  let payout = 0;
-  const randomNumber = getWeightedRandomNumber();
+	const dice = document.querySelector('.dice');
+	const betType = document.querySelector('input[name="bet-type"]:checked').value;
+	const betAmount = document.getElementById('bet-amount').value;
+	let payout = 0;
+	const randomNumber = getWeightedRandomNumber();
+    dice.textContent = randomNumber;
+    dice.classList.remove('animate__animated', 'animate__backInRight');
+    setTimeout(() => {
+        dice.classList.add('animate__animated', 'animate__backInRight');
+    }, 10);
+    
 
+	if (betType === 'lower') {
+		if (randomNumber < 4) {
+			payout = betAmount * 0.7;
+		}
+	} else if (betType === 'higher') {
+		if (randomNumber > 3) {
+			payout = betAmount * 0.7;
+		}
+	} else if (betType === 'zero') {
+		if (randomNumber == 0) {
+			payout = betAmount * 4;
+		}
+	}
 
-  dice.textContent = randomNumber;
-
-  if (betType === 'lower') {
-    if (randomNumber < 4) {
-      payout = betAmount * 0.7;
-    }
-  } else if (betType === 'higher') {
-    if (randomNumber > 3) {
-      payout = betAmount * 0.7;
-    }
-  } else if (betType === 'zero') {
-    if (randomNumber == 0) {
-      payout = betAmount * 4;
-    }
-  }
-
-  updateBalance(payout);
+	updateBalance(payout);
 });
 
 function getWeightedRandomNumber() {
-  const probabilityList = [0, 1, 2, 3, 4, 5, 6];
-  const weights = [10, 15, 15, 15, 15, 15, 15];
-  const weightedList = [];
+	const probabilityList = [0, 1, 2, 3, 4, 5, 6];
+	const weights = [10, 15, 15, 15, 15, 15, 15];
+	const weightedList = [];
 
-  for (let i = 0; i < probabilityList.length; i++) {
-    for (let j = 0; j < weights[i]; j++) {
-      weightedList.push(probabilityList[i]);
-    }
-  }
+	for (let i = 0; i < probabilityList.length; i++) {
+		for (let j = 0; j < weights[i]; j++) {
+			weightedList.push(probabilityList[i]);
+		}
+	}
 
-  const randomIndex = Math.floor(Math.random() * weightedList.length);
-  const randomNumber = weightedList[randomIndex];
+	const randomIndex = Math.floor(Math.random() * weightedList.length);
+	const randomNumber = weightedList[randomIndex];
 
-  return randomNumber;
+	return randomNumber;
 }
 
 function updateBalance(payout) {
-  const balanceDisplay = document.getElementById('balance');
-  const currentBalance = Number(balanceDisplay.textContent);
-  const newBalance = currentBalance + payout;
-  balanceDisplay.textContent = newBalance;
+	const balanceDisplay = document.getElementById('balance');
+	const currentBalance = Number(balanceDisplay.textContent);
+	const newBalance = currentBalance + payout;
+	balanceDisplay.textContent = newBalance;
 }
-const placeBetButton = document.getElementById('place-bet');
-placeBetButton.addEventListener('click', function() {
-  const betAmount = document.getElementById('bet-amount').value;
-  updateBalance(-betAmount);
-});
